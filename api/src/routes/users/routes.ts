d4ba87d283e70b19676ef3d7413bc23db5830306
liujip0 <liujip2020@gmail.com>
2025-08-23 19:41:43 -0400

    maybe this fixes deploy

--- api/src/routes/users/routes.ts
+++ api/src/routes/users/routes.ts
@@ -1,5 +1,5 @@
 import { router } from "../../trpc.ts";
-import { checkUsername } from "./checkUsername.ts";
+import { checkUsername } from "./checkusername.ts";
 import { login } from "./login.ts";
 import { signup } from "./signup.ts";
 import { userInfo } from "./userInfo.ts";
